89c024b5bd291a00ba4c1de3a8c97b124a488ee9
Thijsvl12 <thijsvl12@gmail.com>
2022-06-05 13:09:41 +0200

    Add Image swiper

--- pages/movie/[id].tsx
+++ pages/movie/[id].tsx
@@ -1,29 +1,44 @@
-import { MovieResponse } from 'moviedb-promise/dist/request-types';
+import { MovieImagesResponse, MovieResponse } from 'moviedb-promise/dist/request-types';
 import type { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import { ParsedUrlQuery } from 'querystring';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 import { connect } from '../../lib/moviedb';
 
 interface Props {
   movie: MovieResponse;
+  images: MovieImagesResponse;
 }
+
 interface Params extends ParsedUrlQuery {
   id: string;
 }
 
-const Movie: NextPage<Props> = ({ movie }) => {
+const Movie: NextPage<Props> = ({ movie, images }) => {
   return (
     <>
       <Head>
         <title>{movie && movie.title} | Movie Time</title>
       </Head>
       <div className="py-5">
-        <div className="grid grid-cols-3 gap-x-4">
-          <div className="relative col-span-1 aspect-[9/16]">
-            <Image src={movie.poster_path ? `https://image.tmdb.org/t/p/original${movie.poster_path}` : '/no_image.webp'} alt={movie.title} layout="fill" objectFit="cover" loading="eager" className="transition-opacity duration-500 group-hover:opacity-75" />
+        <div className="grid grid-cols-1 gap-y-4 md:grid-cols-9 md:gap-x-4">
+          <div className="relative col-span-2 aspect-[9/16] overflow-hidden rounded-lg shadow">
+            <Image src={movie.poster_path ? `https://image.tmdb.org/t/p/original${movie.poster_path}` : '/no_image.webp'} alt={movie.title} layout="fill" objectFit="cover" loading="eager" />
           </div>
-          <div className="relative col-span-2 aspect-[19/16]"></div>
+          <Swiper className="relative col-span-7 aspect-[16/9] w-full overflow-hidden rounded-lg shadow">
+            {images.backdrops?.map((image, key) => (
+              <SwiperSlide key={key}>
+                <Image src={`https://image.tmdb.org/t/p/original${image.file_path}`} alt={movie.title} layout="fill" objectFit="cover" loading="eager" />
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
       </div>
     </>
@@ -44,9 +59,12 @@ export const getStaticProps: GetStaticProps = async (context) => {
 
   if (!movie.id) return { notFound: true };
 
+  const images = await connect().movieImages({ id: id });
+
   return {
     props: {
       movie,
+      images,
     },
     revalidate: 86400,
   };
