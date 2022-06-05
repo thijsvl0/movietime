58a32a0591490e137447bedea7cc2e8b86731040
Thijsvl12 <thijsvl12@gmail.com>
2022-06-05 22:08:30 +0200

    Home add rating

--- components/home/MovieListItem.tsx
+++ components/home/MovieListItem.tsx
@@ -1,3 +1,4 @@
+import { StarIcon } from '@heroicons/react/solid';
 import { MovieResult } from 'moviedb-promise/dist/request-types';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -14,8 +15,12 @@ export const MovieListItem: React.FC<Props> = ({ movie }) => {
         <div className="relative aspect-square w-full overflow-hidden rounded-lg rounded-b-none ">
           <Image src={movie.poster_path ? `https://image.tmdb.org/t/p/original${movie.poster_path}` : '/no_image.webp'} alt={movie.title} layout="fill" objectFit="cover" loading="eager" className="transition-opacity duration-500 group-hover:opacity-75" />
         </div>
-        <div className="px-3 py-4 sm:px-6">
-          <h3 className="mt-2 text-sm text-gray-700">{movie.release_date ? movie.release_date : 'To be announced'}</h3>
+        <div className="px-3 py-4 pt-2 sm:px-6">
+          <span className="inline-flex items-center text-yellow-300">
+            <StarIcon className="mr-1 inline-block h-4" />
+            {movie.vote_average}
+          </span>
+          <h3 className="text-sm text-gray-700">{movie.release_date ? movie.release_date : 'To be announced'}</h3>
           <p className="mt-1 text-base font-medium text-gray-900">{movie.title}</p>
         </div>
       </div>
