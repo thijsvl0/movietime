5fdfb5843970fdb1183e19171f56ef8710524866
Thijsvl12 <thijsvl12@gmail.com>
2022-06-06 18:10:23 +0200

    Add Umami analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <script async defer data-website-id="6098b96d-644b-435f-80df-23fc465180a2" src="https://analytics.yeboii.com/umami.js"></script>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
